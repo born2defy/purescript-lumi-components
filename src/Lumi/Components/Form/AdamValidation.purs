3db18f20c7799253460ae24e1b27b6ae3b079c9b
Adam Rosenblatt <born2defy@gmail.com>
2020-05-18 20:00:31 -0400

    Isolated changes to one file

--- src/Lumi/Components/Form/AdamValidation.purs
+++ src/Lumi/Components/Form/AdamValidation.purs
@@ -1,28 +1,8 @@
-module Lumi.Components.Form.Validation2
-  ( Validator
-  , nonEmpty, nonEmptyArray, nonNull
-  , mustEqual, mustBe
-  , validNumber, validInt, validDate
-  , optional
-  , Validated(..)
-  , _Validated, _Fresh, _Modified
-  , setFresh, setModified
-  , ModifyValidated(..)
-  , class CanValidate, fresh, modified, fromValidated
-  , validated
-  , warn
-  , FormField(..)
-  , FieldState(..)
-  , ValidationState(..)
-  , fieldState
-  , formValue
-  , InputEvent(..)
-  , updateFormField
-  , validated2
-  ) where
+module Lumi.Components.Form.AdamValidation where
 
 import Prelude
 
+import Data.Array (elem)
 import Data.Array as Array
 import Data.Array.NonEmpty (NonEmptyArray)
 import Data.Array.NonEmpty (fromArray) as NEA
@@ -30,10 +10,9 @@ import Data.Date as Date
 import Data.Either (Either(..), either, hush, note)
 import Data.Enum (toEnum)
 import Data.Eq (class Eq1)
-import Data.Foldable (foldMap)
+import Data.Foldable (foldMap, traverse_)
 import Data.Int as Int
-import Data.Lens (Lens, Prism', lens, over, prism', review, view)
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), isNothing)
 import Data.Monoid (guard)
 import Data.Newtype (class Newtype, un)
 import Data.Nullable (notNull)
@@ -46,12 +25,13 @@ import Data.String.Pattern (Pattern(..))
 import Data.Traversable (traverse)
 import Heterogeneous.Mapping (class MapRecordWithIndex, class Mapping, ConstMapping, hmap, mapping)
 import Lumi.Components.Column (column)
-import Lumi.Components.Form.Internal (Forest, FormBuilder, FormBuilder'(..), Tree(..))
+import Lumi.Components.Form.Internal (Forest, FormBuilder, FormBuilder'(..), Tree(..), formBuilder)
+import Lumi.Components.Input as Input
 import Lumi.Components.LabeledField (ValidationMessage(..))
-import Lumi.Components.Text (subtext, text)
+import Lumi.Components.Text (body_, subtext, text)
 import Prim.RowList as RL
-import React.Basic (JSX)
 import React.Basic.DOM as R
+import React.Basic.DOM.Events (capture, targetValue)
 
 -- | A `Validator` takes a possibly invalid form `result` and produces
 -- | a `valid` result, or an error message.
@@ -111,14 +91,10 @@ optional :: forall a. Validator String a -> Validator String (Maybe a)
 optional _ "" = pure Nothing
 optional v s  = map Just (v s)
 
+
+
+-- | FORMFIELDS
 newtype FormField a = FormField {fieldState :: FieldState, value :: a}  
-
-fieldState :: ∀ a. FormField a -> FieldState
-fieldState = _.fieldState <<< un FormField 
-
-formValue :: ∀ a. FormField a -> a
-formValue = _.value <<< un FormField 
-
 derive instance ntFormField :: Newtype (FormField a) _
 derive instance eqeqFormField :: Eq a => Eq (FormField a)
 derive instance eq1FormField :: Eq1 FormField
@@ -143,6 +119,21 @@ instance applyFormField :: Apply FormField where
 
 instance applicativeFormField :: Applicative FormField where
   pure x = FormField {fieldState: Initial, value: x } 
+
+fieldState :: ∀ a. FormField a -> FieldState
+fieldState = _.fieldState <<< un FormField 
+
+formValue :: ∀ a. FormField a -> a
+formValue = _.value <<< un FormField 
+
+updateFieldState :: ∀ a. FieldState -> FormField a -> FormField a 
+updateFieldState fs (FormField ff) = FormField ff {fieldState = fs}
+
+-- | Update validation status of a formfield which has been modified
+updateValidation :: ∀ a.  ValidationState -> FormField a -> FormField a 
+updateValidation v ff = case fieldState ff of
+  BeenModified _ -> updateFieldState (BeenModified v) ff
+  _              -> ff
 
 data InputEvent a = ChangeEvent a | BlurEvent a 
 derive instance eqeqInputEvent :: Eq a => Eq (InputEvent a)
@@ -196,12 +187,43 @@ instance sgValidationState :: Semigroup ValidationState where
     _,              BeingValidated -> BeingValidated
     BeingValidated, _              -> BeingValidated
 
-validated2
+-- | Internal utility type for modifying the validated state of fields in
+-- | records containing `Validated` values.
+newtype ModifyFormField = ModifyFormField (FormField ~> FormField)
+
+instance modifyFormField :: Mapping ModifyFormField a a => Mapping ModifyFormField (FormField a) (FormField a) where
+  mapping m@(ModifyFormField f) = map (mapping m) <<< f
+else instance modifyFormFieldRecord :: (RL.RowToList r xs, MapRecordWithIndex xs (ConstMapping ModifyFormField) r r) => Mapping ModifyFormField {| r} {| r} where
+  mapping d = hmap d
+else instance modifyFormFieldArray :: Mapping ModifyFormField a a => Mapping ModifyFormField (Array a) (Array a) where
+  mapping d = map (mapping d)
+else instance modifyFormFieldIdentity :: Mapping ModifyFormField a a where
+  mapping _ = identity
+
+-- | Sets all `Validated` fields in a record to `Fresh`, hiding all validation
+-- | messages.
+setInitial
+  :: forall value
+   . Mapping ModifyFormField value value
+  => value
+  -> value
+setInitial = mapping (ModifyFormField (updateFieldState Initial))
+
+-- | Sets all `Validated` fields in a record to `Modified`, showing all
+-- | validation messages.
+setBeenModified
+  :: forall value
+   . Mapping ModifyFormField value value
+  => value
+  -> value
+setBeenModified = mapping (ModifyFormField (updateFieldState $ BeenModified Valid))
+
+validated
   :: ∀ props validated a b
    . Validator a b
-  -> FormBuilder { readonly :: Boolean, waitToWarn :: Boolean | props } (FormField validated) a
-  -> FormBuilder { readonly :: Boolean, waitToWarn :: Boolean | props } (FormField validated) b
-validated2 runValidator editor = FormBuilder \props@{ readonly, waitToWarn } ff ->
+  -> FormBuilder { readonly :: Boolean, waitToShowErrors :: Boolean | props } (FormField validated) a
+  -> FormBuilder { readonly :: Boolean, waitToShowErrors :: Boolean | props } (FormField validated) b
+validated runValidator editor = FormBuilder \props@{ readonly, waitToShowErrors } ff ->
   let
       innerColumn_ children =
         column
@@ -244,9 +266,9 @@ validated2 runValidator editor = FormBuilder \props@{ readonly, waitToWarn } ff 
         case fieldState ff of
           Initial ->
             hushError valid
-          BeingModifiedFirstTime | waitToWarn ->
+          BeingModifiedFirstTime | waitToShowErrors ->
             hushError valid
-          BeingModifiedAgain | waitToWarn ->
+          BeingModifiedAgain | waitToShowErrors ->
             hushError valid
           _ -> 
             showError valid
@@ -255,226 +277,111 @@ validated2 runValidator editor = FormBuilder \props@{ readonly, waitToWarn } ff 
       showError valid = pure $ runValidator valid
       err = either pure (const Nothing) =<< res
 
-   in { edit: \onChange -> modify err <<< edit $ onChange 
+   in { edit: \onChange -> (modify err <<< edit) (onChange <<< \f -> f <<< (if isNothing err then identity else updateValidation Invalid))
       , validate: hush =<< res
       }
 
--- | The `Validated` type describes the state of a validated form field. This
--- | state may be used to modify the way this form field or its validation
--- | messages are displayed.
--- |
--- | TODO: maybe convert this type to a record? Possible extensions to this
--- | type (as a record) could be a field `valid :: Boolean` to display an
--- | indicator that the field is valid, or a field
--- | `validating :: Maybe (Canceler a)` to control form fields with asynchronous
--- | validation.
-data Validated a
-  = Fresh a
-  | Modified a
+-- | A configurable input box makes a `FormBuilder` for strings
+inputBox
+  :: forall props
+   . Input.InputProps
+  -> FormBuilder
+       { readonly :: Boolean
+       , waitToShowErrors :: Boolean
+      -- ^ Rather than show errors on first edit, we wait until the user has entered their first value before showing errors
+       , newValueOnlyOnFocusChange:: Boolean
+      -- ^ this means that new values are only produced on Blur Events  
+       | props 
+       }
+       (FormField String)
+       String
+inputBox inputProps = formBuilder \{ readonly, newValueOnlyOnFocusChange } ff ->
+  let edit onChange = if readonly
+        then Input.alignToInput $ body_ (formValue ff)
+        else Input.input inputProps
+              { value = formValue ff
+              , onChange = capture targetValue $ traverse_ (onChange <<< updateFormField <<< ChangeEvent)
+              , onBlur = notNull $ capture targetValue $ traverse_ (onChange <<< updateFormField <<< BlurEvent)
+              , style = R.css { width: "100%" }
+              }
+  in { edit
+     , validate: 
+        if newValueOnlyOnFocusChange && (fieldState ff `elem` [BeingModifiedFirstTime,BeingModifiedAgain]) 
+      -- ^ Here we restrict the production of new values only on blur-events 
+      -- | TODO: Do we need to add return key presses?
+          then Nothing
+          else Just (formValue ff)
+    }
 
-derive instance eqValidated :: Eq a => Eq (Validated a)
-derive instance eq1Validated :: Eq1 Validated
-derive instance ordValidated :: Ord a => Ord (Validated a)
-derive instance ord1Validated :: Ord1 Validated
+-- | A simple text box makes a `FormBuilder` for strings
+textbox
+  :: forall props
+   . FormBuilder
+      { readonly :: Boolean, waitToShowErrors :: Boolean, newValueOnlyOnFocusChange :: Boolean | props }
+       (FormField String)
+       String
+textbox = inputBox Input.text_
 
-derive instance functorValidated :: Functor Validated
+-- | A simple password box makes a `FormBuilder` for strings
+passwordBox
+  :: forall props
+   . FormBuilder
+      { readonly :: Boolean, waitToShowErrors :: Boolean, newValueOnlyOnFocusChange :: Boolean | props }
+       (FormField String)
+       String
+passwordBox = inputBox Input.password
 
-instance applyValidated :: Apply Validated where
-  apply (Fresh f) r = f <$> r
-  apply (Modified f) (Fresh a) = Modified (f a)
-  apply (Modified f) (Modified a) = Modified (f a)
-
-instance applicativeValidated :: Applicative Validated where
-  pure = Fresh
-
--- | Lens for viewing and modifying `Validated` values.
-_Validated :: forall a b. Lens (Validated a) (Validated b) a b
-_Validated = flip lens ($>) $
-  case _ of
-    Fresh a -> a
-    Modified a -> a
-
--- | Prism for the `Fresh` constructor of `Validated`.
-_Fresh :: forall a. Prism' (Validated a) a
-_Fresh = prism' Fresh $
-  case _ of
-    Fresh a -> Just a
-    _ -> Nothing
-
--- | Prism for the `Modified` constructor of `Validated`.
-_Modified :: forall a. Prism' (Validated a) a
-_Modified = prism' Modified $
-  case _ of
-    Modified a -> Just a
-    _ -> Nothing
-
--- | Sets all `Validated` fields in a record to `Fresh`, hiding all validation
--- | messages.
-setFresh
-  :: forall value
-   . Mapping ModifyValidated value value
-  => value
-  -> value
-setFresh = mapping (ModifyValidated (Fresh <<< view _Validated))
-
--- | Sets all `Validated` fields in a record to `Modified`, showing all
--- | validation messages.
-setModified
-  :: forall value
-   . Mapping ModifyValidated value value
-  => value
-  -> value
-setModified = mapping (ModifyValidated (Modified <<< view _Validated))
-
--- | Internal utility type for modifying the validated state of fields in
--- | records containing `Validated` values.
-newtype ModifyValidated = ModifyValidated (Validated ~> Validated)
-
-instance modifyValidated :: Mapping ModifyValidated a a => Mapping ModifyValidated (Validated a) (Validated a) where
-  mapping m@(ModifyValidated f) = over _Validated (mapping m) <<< f
-else instance modifyValidatedRecord :: (RL.RowToList r xs, MapRecordWithIndex xs (ConstMapping ModifyValidated) r r) => Mapping ModifyValidated {| r} {| r} where
-  mapping d = hmap d
-else instance modifyValidatedArray :: Mapping ModifyValidated a a => Mapping ModifyValidated (Array a) (Array a) where
-  mapping d = map (mapping d)
-else instance modifyValidatedIdentity :: Mapping ModifyValidated a a where
-  mapping _ = identity
-
--- | Internal utility type class used to flatten repeated applications of
--- | `Validated` to a type.
-class CanValidate u v | u -> v where
-  fresh :: Prism' (Validated v) u
-  modified :: Prism' (Validated v) u
-  fromValidated :: Validated v -> u
-
-instance canValidateValidated :: CanValidate (Validated a) a where
-  fresh = identity
-  modified = identity
-  fromValidated = identity
-else instance canValidateAny :: CanValidate a a where
-  fresh = _Fresh
-  modified = _Modified
-  fromValidated = view _Validated
-
--- | Attach a validation function to a `FormBuilder p u a`, producing a new
--- | `FormBuilder` that takes a `Validated u` as form state and displays an
--- | error message if its form data is invalid.
--- |
--- | This `Validated` data type describes a form field as either `Fresh` or
--- | `Modified`, so that validation messages are only displayed if the field
--- | is `Modified`.
-validated
-  :: forall props unvalidated validated result result_
-   . CanValidate unvalidated validated
-  => Validator result_ result
-  -> FormBuilder { readonly :: Boolean | props } unvalidated result_
-  -> FormBuilder { readonly :: Boolean | props } (Validated validated) result
-validated runValidator editor = FormBuilder \props@{ readonly } v ->
-  let value = fromValidated v
-
-      innerColumn_ children =
-        column
-          { style: R.css { maxWidth: "100%", maxHeight: "100%" }
-          , children
-          }
-
-      { edit, validate } = un FormBuilder editor props value
-
-      modify :: Maybe String -> Forest -> Forest
-      modify message forest =
-          case Array.unsnoc forest of
-            Nothing -> [Child { key: Nothing, child: errLine }]
-            Just { init, last: Child c } ->
-              Array.snoc init (Child c { child = innerColumn_ [c.child, errLine] })
-            Just { init, last: Wrapper c } ->
-              Array.snoc init (Wrapper c { children = modify message c.children })
-            Just { init, last: Node n } ->
-              Array.snoc init (Node n { validationError = Error <$> message })
-        where
-          errLine =
-            guard (not readonly) message # foldMap \s ->
-              case Error s of
-                Error e ->
-                  text subtext
-                    { className = notNull "labeled-field--validation-error"
-                    , children = [ R.text e ]
-                    }
-                Warning w ->
-                  text subtext
-                    { className = notNull "labeled-field--validation-warning"
-                    , children = [ R.text w ]
-                    }
-
-      -- The validation can produce either a valid result, an error message, or
-      -- none in the case where the form is Fresh.
-      res :: Maybe (Either String result)
-      res = do
-        valid <- validate
-        case v of
-          Fresh _ ->
-            pure <$> hush (runValidator valid)
-          _ ->
-            pure $ runValidator valid
-
-      err = either pure (const Nothing) =<< res
-
-   in { edit: \onChange -> (modify err <<< edit) (onChange <<< \f ->
-          case _ of
-            v'@(Fresh u) -> review modified (f (fromValidated v'))
-            v'@(Modified u) -> review modified (f (fromValidated v'))
-        )
-      , validate: hush =<< res
-      }
 
 -- | Attach a validation function to a `FormBuilder p u a`, producing a new
 -- | `FormBuilder` that takes a `Validated u` as form state and displays a
 -- | warning message if its form data triggers a warning, while still allowing
 -- | the form to proceed.
-warn
-  :: forall props unvalidated validated result
-   . CanValidate unvalidated validated
-  => WarningValidator result
-  -> FormBuilder { readonly :: Boolean | props } unvalidated result
-  -> FormBuilder { readonly :: Boolean | props } (Validated validated) result
-warn warningValidator editor = FormBuilder \props@{ readonly } v ->
-  let { edit, validate } = un FormBuilder editor props (fromValidated v)
+-- warn
+--   :: forall props unvalidated validated result
+--    . CanValidate unvalidated validated
+--   => WarningValidator result
+--   -> FormBuilder { readonly :: Boolean | props } unvalidated result
+--   -> FormBuilder { readonly :: Boolean | props } (Validated validated) result
+-- warn warningValidator editor = FormBuilder \props@{ readonly } v ->
+--   let { edit, validate } = un FormBuilder editor props (fromValidated v)
 
-      innerColumn_ children =
-        column
-          { style: R.css { maxWidth: "100%", maxHeight: "100%" }
-          , children
-          }
+--       innerColumn_ children =
+--         column
+--           { style: R.css { maxWidth: "100%", maxHeight: "100%" }
+--           , children
+--           }
 
-      modify :: Forest -> Forest
-      modify forest =
-          case Array.unsnoc forest of
-            Nothing -> [Child { key: Nothing, child: errLine }]
-            Just { init, last: Child c } ->
-              Array.snoc init (Child c { child = innerColumn_ [c.child, errLine] })
-            Just { init, last: Wrapper c } ->
-              Array.snoc init (Wrapper c { children = modify c.children })
-            Just { init, last: Node n } ->
-              Array.snoc init (Node n { validationError = Warning <$> message })
+--       modify :: Forest -> Forest
+--       modify forest =
+--           case Array.unsnoc forest of
+--             Nothing -> [Child { key: Nothing, child: errLine }]
+--             Just { init, last: Child c } ->
+--               Array.snoc init (Child c { child = innerColumn_ [c.child, errLine] })
+--             Just { init, last: Wrapper c } ->
+--               Array.snoc init (Wrapper c { children = modify c.children })
+--             Just { init, last: Node n } ->
+--               Array.snoc init (Node n { validationError = Warning <$> message })
 
-      errLine :: JSX
-      errLine =
-        guard (not readonly) message # foldMap \s ->
-          text subtext
-            { className = notNull "labeled-field--validation-warning"
-            , children = [ R.text s ]
-            }
+--       errLine :: JSX
+--       errLine =
+--         guard (not readonly) message # foldMap \s ->
+--           text subtext
+--             { className = notNull "labeled-field--validation-warning"
+--             , children = [ R.text s ]
+--             }
 
-      message :: Maybe String
-      message =
-        case v of
-          Fresh _ ->
-            Nothing
-          _ ->
-            warningValidator =<< validate
+--       message :: Maybe String
+--       message =
+--         case v of
+--           Fresh _ ->
+--             Nothing
+--           _ ->
+--             warningValidator =<< validate
 
-   in { edit: \onChange -> (modify <<< edit) (onChange <<< \f ->
-          case _ of
-            v'@(Fresh u) -> review modified (f (fromValidated v'))
-            v'@(Modified u) -> review modified (f (fromValidated v'))
-        )
-      , validate
-      }
+--    in { edit: \onChange -> (modify <<< edit) (onChange <<< \f ->
+--           case _ of
+--             v'@(Fresh u) -> review modified (f (fromValidated v'))
+--             v'@(Modified u) -> review modified (f (fromValidated v'))
+--         )
+--       , validate
+--       }
